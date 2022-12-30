b5b47d027314abfd35bee6d76bc0620d7729c694
Yashwanth142 <119283440+Yashwanth142@users.noreply.github.com>
2022-12-30 16:11:30 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,8 +7,11 @@
             Console.WriteLine("Welcome to Line Comparison Computation Program");
 
             LineComparsion obj = new LineComparsion();
-            //obj.LineLength();
-            //obj.LineEqual();
+            
+            obj.LineLength();
+            
+            obj.LineEqual();
+            
             obj.Linecompare();
 
         }
